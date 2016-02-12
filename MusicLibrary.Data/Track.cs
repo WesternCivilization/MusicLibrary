5c89620ba3551395a07088fb120743ea703a2dfd
Zachary Maillard <zmaillard@resdat.com>
2016-02-12 07:28:30 -0700

    Genre and artist repository completed

---
+++ MusicLibrary.Data/Track.cs
@@ -0,0 +1,9 @@
+namespace MusicLibrary.Data
+{
+    public class Track
+    {
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public int Number { get; set; }
+    }
+}
