ac08a153a89137621b2edf6c082682cfe0ab69fe
Zachary Maillard <zmaillard@resdat.com>
2016-02-12 16:01:22 -0700

    WOrking through repositories

--- MusicLibrary.DataAccess/AlbumRepository.cs
+++ MusicLibrary.DataAccess/AlbumRepository.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 using MusicLibrary.Data;
 using MusicLibrary.DataAccess.Interfaces;
 
@@ -13,14 +14,58 @@ namespace MusicLibrary.DataAccess
     {
         private readonly IDbConnection _database;
 
+
+        private readonly string _baseQuery = @"SELECT 
+album.Name as AlbumName, artist.Name as ArtistName, genre.Name as GenreName,
+album.Year as ReleaseYear, track.Length, track.Name, track.TrackNumber as Number
+FROM dbo.Album album INNER JOIN dbo.Artist artist
+ON album.ArtistId = artist.Id
+INNER JOIN dbo.Genre genre
+ON album.GenreId = genre.Id
+INNER JOIN dbo.xrAlbumTrack xr ON
+album.Id = xr.AlbumId
+INNER JOIN dbo.Track track ON
+xr.TrackId = track.Id";
+
         public AlbumRepository(IDbConnection database)
         {
             _database = database;
         }
 
+
+
         public Album GetAlbumByName(string name)
         {
-            throw new NotImplementedException();
+            var sql = _baseQuery + " WHERE album.Name=@albumName";
+
+            Album album = null;
+            IList<Track> tracks = new List<Track>();
+            foreach (var d in  _database.Query(sql, new {albumName = name}))
+            {
+                if (album == null)
+                {
+                    album = new Album();
+                    album.AlbumName = d.AlbumName;
+                    album.ReleaseYear = d.ReleaseYear;
+                    album.Genre = new Genre {GenreName = d.GenreName};
+                    album.Artist = new Artist {AristName = d.ArtistName};
+                    album.Tracks = new List<Track>();
+                }
+
+                tracks.Add(new Track
+                {
+                    Length = d.Length,
+                    Name = d.Name,
+                    Number = d.Number
+                });
+
+
+            }
+
+            album.Tracks = tracks;
+
+            return album;
+          
         }
 
         public IEnumerable<Album> FindAllAlbumsByGenre(Data.Genre genre)
