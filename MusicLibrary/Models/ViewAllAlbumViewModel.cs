fdbea69b08787c7ca7aa70008cbde8d56df8fbc0
Zachary Maillard <zmaillard@resdat.com>
2016-02-15 06:28:57 -0700

    Adding view models

---
+++ MusicLibrary/Models/ViewAllAlbumViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicLibrary.Models
+{
+    public class ViewAllAlbumViewModel
+    {
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+
+        public int AlbumLength { get; set; }
+
+        public IEnumerable<TrackViewModel> Tracks { get; set; } 
+    }
+}
